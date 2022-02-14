24d76a1842946f4fb8f6da7cf8bac84b9b87c7b9
sumanchandradas <suman.chandra.das@gmail.com>
2022-02-14 22:37:08 +0600

    handle event in withdraw button and balance calculate

--- js/banking.js
+++ js/banking.js
@@ -20,4 +20,33 @@ document.getElementById("diposit-button").addEventListener("click", function () 
 
     dipositInput.value = '';
 
+});
+
+// handle withdraw button 
+document.getElementById("withdraw-button").addEventListener("click", function () {
+    // geting withdraw amount from user
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputValue = parseFloat(withdrawInput.value);
+
+    // get withdraw field for put value 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalValue = parseFloat(withdrawTotal.innerText);
+
+    let totalWithdraw = withdrawTotalValue + withdrawInputValue;
+    withdrawTotal.innerText = totalWithdraw;
+    // calculate balance after withdraw 
+    const totalBalance = document.getElementById('balance-total');
+    const totalBalanceValue = parseFloat(totalBalance.innerText);
+
+
+    // calculate balance 
+    let newBalance = totalBalanceValue - withdrawInputValue;
+    totalBalance.innerText = newBalance;
+
+    // cleare value from input filed after button clicked 
+    withdrawInput.value = '';
+
+
+
+
 })
